bb782ecb0be640daedb17ff13d1f1b35bfa479d5
ankitkumarojha <ankitkumarojha2@gmail.com>
2017-01-23 23:03:58 +0530

    Added Computer Turn //TODO-Delay

--- Unit-2/ScarneDice/app/src/main/java/com/example/root/scarnedice/MainActivity.java
+++ Unit-2/ScarneDice/app/src/main/java/com/example/root/scarnedice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.root.scarnedice;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +22,31 @@ public class MainActivity extends AppCompatActivity {
     public int currentDiceValue = 0;
     public boolean userTurn = true; //1 is user & 0 is computer // user starts the game always
     public boolean holdButtonPressed = false; //0 is no & 1 is yes
+  //  long startTime = 0;
 
+   /* //runs without a timer by reposting this handler at the end of the runnable
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+        updateUserTurnText();
+    }
+*/
     public void updateTextView()
     {
         TextView textView = (TextView) findViewById(R.id.scoreView);
@@ -52,122 +78,32 @@ public class MainActivity extends AppCompatActivity {
             holdText.setText("Next: User's Turn");
         else
             holdText.setText("Next: Computer's Turn");
+
     }
 
+
+    Handler delay;
     public void computerTurn()
     {
 
         //Log.d("Computer turn","Function called");
+
         Button rollButton = (Button)findViewById(R.id.rollButton);
         final TextView winTextView = (TextView) findViewById(R.id.winText);
         final Button holdButton = (Button)findViewById(R.id.holdButton);
-
+        holdButton.setEnabled(false);
+        rollButton.setEnabled(false);
         Random random = new Random();
-        int p = random.nextInt(1);
-
-        //if(p==0)
-        //{
-        //    rollButton.performClick();
-        //}
-        //else
-        // {
-        //    holdButton.performClick();
-        // }
-
-        rollButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //updateAction("Scarne's Dice");
-                Log.d("Computer Turn","Dice Rolled");
-                Random random = new Random();
-                currentDiceValue = random.nextInt(MAX_DICE_VALUE-1)+1;
-                Log.d("Dice Value",""+currentDiceValue);
-                ImageView diceImage = (ImageView)findViewById(R.id.imageView);
-                if(currentDiceValue == 1)
-                {
-                    diceImage.setImageResource(R.drawable.dice1);
-                }
-                if(currentDiceValue == 2)
-                {
-                    diceImage.setImageResource(R.drawable.dice2);
-                }
-                if(currentDiceValue == 3)
-                {
-                    diceImage.setImageResource(R.drawable.dice3);
-                }
-                if(currentDiceValue == 4)
-                {
-                    diceImage.setImageResource(R.drawable.dice4);
-                }
-                if(currentDiceValue == 5)
-                {
-                    diceImage.setImageResource(R.drawable.dice5);
-                }
-                if(currentDiceValue == 6)
-                {
-                    diceImage.setImageResource(R.drawable.dice6);
-                }
-                if(userTurn)
-                {
-                    userTurnScore+=currentDiceValue;
-                }
-                else
-                {
-                    compTurnScore+=currentDiceValue;
-                }
-                if(currentDiceValue == 1)
-                {
-                    updateAction("rolled one");
-                    userTurn=!userTurn;
-                    userTurnScore=0;
-                    compTurnScore=0;
-                    updateTextView();
-                    Log.d("1 facing up","Turn values Reset");
-
-                }
-                updateTextView();
-                updateUserTurnText();
-            }
-
-        });
-        holdButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateTextView();
-                userOverallScore += userTurnScore;
-                compOverallScore += compTurnScore;
-                updateTextView();
-                userTurnScore = 0;
-                compTurnScore = 0;
-                updateAction("holds");
-                userTurn = !userTurn;
-                //if userturn 0 then call computer turn here.
-                updateTextView();
-                if(userOverallScore >= 100 || compOverallScore >= 100)
-                {
-                    winTextView.setVisibility(View.VISIBLE);
-                    if(userOverallScore >= 100)
-                        winTextView.setText("USER WINS");
-                    else
-                        winTextView.setText("COMPUTER WINS");
-
-                    userOverallScore = 0;
-                    userTurnScore = 0;
-                    compOverallScore = 0;
-                    compTurnScore = 0;
-                    currentDiceValue = 0;
-                    //userTurn=true;
-                    updateAction("Scarne\'s Dice");
-                    updateUserTurnText();
-                }
-                updateUserTurnText();
-                Log.d("TextView", "Updated on click of roll button");
-                updateUserTurnText();
-            }
-        });
-
-
+        int numberOfTurns=random.nextInt(6);//at max computer will go for 6 turns before hold
+        for(int i=0;i<numberOfTurns;i++) {
+            roll();
+        }
+            hold();
+        userTurn=true;
+        holdButton.setEnabled(true);
+        rollButton.setEnabled(true);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -177,123 +113,147 @@ public class MainActivity extends AppCompatActivity {
         Button rollButton = (Button)findViewById(R.id.rollButton);
         final Button holdButton = (Button)findViewById(R.id.holdButton);
         final Button resetButton = (Button)findViewById(R.id.resetButton);
-        final TextView winTextView = (TextView) findViewById(R.id.winText);
+        TextView winTextView = (TextView) findViewById(R.id.winText);
 
         winTextView.setVisibility(View.INVISIBLE);
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //updateAction("Scarne's Dice");
-                Random random = new Random();
-                currentDiceValue = random.nextInt(MAX_DICE_VALUE-1)+1;
-                Log.d("Dice Value",""+currentDiceValue);
-                ImageView diceImage = (ImageView)findViewById(R.id.imageView);
-                if(currentDiceValue == 1)
-                {
-                    diceImage.setImageResource(R.drawable.dice1);
-                }
-                if(currentDiceValue == 2)
-                {
-                    diceImage.setImageResource(R.drawable.dice2);
-                }
-                if(currentDiceValue == 3)
-                {
-                    diceImage.setImageResource(R.drawable.dice3);
-                }
-                if(currentDiceValue == 4)
-                {
-                    diceImage.setImageResource(R.drawable.dice4);
-                }
-                if(currentDiceValue == 5)
-                {
-                    diceImage.setImageResource(R.drawable.dice5);
-                }
-                if(currentDiceValue == 6)
-                {
-                    diceImage.setImageResource(R.drawable.dice6);
-                }
-                if(userTurn)
-                {
-                    userTurnScore+=currentDiceValue;
-                }
-                else
-                {
-                    compTurnScore+=currentDiceValue;
-                }
-                if(currentDiceValue == 1)
-                {
-                    updateAction("rolled one");
-                    userTurn=!userTurn;
-                    userTurnScore=0;
-                    compTurnScore=0;
-                    updateTextView();
-                    Log.d("1 facing up","Turn values Reset");
-
-                }
-                updateTextView();
-                updateUserTurnText();
+                roll();
             }
-
         });
 
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userOverallScore = 0;
-                userTurnScore = 0;
-                compOverallScore = 0;
-                compTurnScore = 0;
-                currentDiceValue = 0;
-                userTurn = true;
-                updateUserTurnText();
-                updateTextView();
-                holdButtonPressed = false;
-                winTextView.setVisibility(View.INVISIBLE);
+                reset();
             }
         });
 
         holdButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateTextView();
-                userOverallScore += userTurnScore;
-                compOverallScore += compTurnScore;
-                updateTextView();
-                userTurnScore = 0;
-                compTurnScore = 0;
-                updateAction("holds");
-                userTurn = !userTurn;
-                //if(!userTurn)
-                //computerTurn();
-                if(!userTurn)
-                {
-                    computerTurn();
-                    Log.d("Called ComputerTurn:"," from userturn");
-                }
-                //if userturn 0 then call computer turn here.
-                updateTextView();
-                if(userOverallScore >= 100 || compOverallScore >= 100)
-                {
-                    winTextView.setVisibility(View.VISIBLE);
-                    if(userOverallScore >= 100)
-                        winTextView.setText("USER WINS");
-                    else
-                        winTextView.setText("COMPUTER WINS");
-
-                    userOverallScore = 0;
-                    userTurnScore = 0;
-                    compOverallScore = 0;
-                    compTurnScore = 0;
-                    currentDiceValue = 0;
-                    //userTurn=true;
-                    updateAction("Scarne\'s Dice");
-                    updateUserTurnText();
-                }
-                updateUserTurnText();
-                Log.d("TextView", "Updated on click of roll button");
-                updateUserTurnText();
+                hold();
             }
         });
+    }
+
+    public void roll()
+    {
+        //Log.d("Rolling","b");
+        //updateAction("Scarne's Dice");
+        Random random = new Random();
+        currentDiceValue = random.nextInt(MAX_DICE_VALUE-1)+1;
+        Log.d("Dice Value",""+currentDiceValue);
+        ImageView diceImage = (ImageView)findViewById(R.id.imageView);
+        if(currentDiceValue == 1)
+        {
+            diceImage.setImageResource(R.drawable.dice1);
+        }
+        if(currentDiceValue == 2)
+        {
+            diceImage.setImageResource(R.drawable.dice2);
+        }
+        if(currentDiceValue == 3)
+        {
+            diceImage.setImageResource(R.drawable.dice3);
+        }
+        if(currentDiceValue == 4)
+        {
+            diceImage.setImageResource(R.drawable.dice4);
+        }
+        if(currentDiceValue == 5)
+        {
+            diceImage.setImageResource(R.drawable.dice5);
+        }
+        if(currentDiceValue == 6)
+        {
+            diceImage.setImageResource(R.drawable.dice6);
+        }
+        if(userTurn)
+        {
+            userTurnScore+=currentDiceValue;
+        }
+        else
+        {
+            compTurnScore+=currentDiceValue;
+        }
+        if(currentDiceValue == 1)
+        {
+            updateAction("rolled one");
+            userTurn=!userTurn;
+            userTurnScore=0;
+            compTurnScore=0;
+            updateTextView();
+            updateUserTurnText();
+            Log.d("Rolled 1","calling"+userTurn);
+            if(!userTurn)
+                computerTurn();
+            Log.d("1 facing up","Turn values Reset");
+
+        }
+        updateTextView();
+        updateUserTurnText();
+    }
+
+    public void hold()
+    {
+        Log.d("Hold","My hands");
+        TextView winTextView = (TextView) findViewById(R.id.winText);
+        updateTextView();
+        userOverallScore += userTurnScore;
+        compOverallScore += compTurnScore;
+        updateTextView();
+        userTurnScore = 0;
+        compTurnScore = 0;
+        updateAction("holds");
+        userTurn = !userTurn;
+        if(!userTurn)
+        {
+            computerTurn();
+            Log.d("Called ComputerTurn:"," from userturn"+userTurn);
+        }
+        //if userturn 0 then call computer turn here.
+        updateTextView();
+        if(userOverallScore >= 100 || compOverallScore >= 100)
+        {
+            winTextView.setVisibility(View.VISIBLE);
+           if(userOverallScore >= 100)
+                winTextView.setText("USER WINS");
+            else
+                winTextView.setText("COMPUTER WINS");
+
+            userOverallScore = 0;
+            userTurnScore = 0;
+            compOverallScore = 0;
+            compTurnScore = 0;
+            currentDiceValue = 0;
+            //userTurn=true;
+            updateAction("Scarne\'s Dice");
+            updateUserTurnText();
+        }
+        updateUserTurnText();
+        Log.d("TextView", "Updated on click of roll button");
+        updateUserTurnText();
+    }
+
+    public void reset()
+    {
+        Button rollButton = (Button)findViewById(R.id.rollButton);
+        final Button holdButton = (Button)findViewById(R.id.holdButton);
+        TextView winTextView = (TextView) findViewById(R.id.winText);
+        userOverallScore = 0;
+        userTurnScore = 0;
+        compOverallScore = 0;
+        compTurnScore = 0;
+        currentDiceValue = 0;
+        userTurn = true;
+        updateUserTurnText();
+        updateTextView();
+        holdButtonPressed = false;
+        winTextView.setVisibility(View.INVISIBLE);
+        holdButton.setEnabled(false);
+        rollButton.setEnabled(false);
     }
 }
